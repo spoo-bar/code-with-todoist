9927602ecdcfa256c67f56cde6a45891a3691443
Pablo Cordero <pablocorderoromero@gmail.com>
2020-09-27 12:37:05 +0200

    Solving crossing out the completed subtask #5

--- src/models/task.ts
+++ src/models/task.ts
@@ -1,5 +1,12 @@
 import { Url } from 'url';
 
+function strikeThroughContent(text: string) {
+    return text
+        .split('')
+        .map( ( char: string ) => char + '\u0336')
+        .join('')
+}
+
 export default class task {
 
     id!: Number;
@@ -16,6 +23,11 @@ export default class task {
     comment_count!: Number;
 
     public static deserialize(json: any) : task {
+
+        // Completed subtasks cross out
+        if(json.completed)
+            json.content = strikeThroughContent(json.content);
+            
         //TODO : Validate before returning
         return Object.assign(new task(), json);        
     }
